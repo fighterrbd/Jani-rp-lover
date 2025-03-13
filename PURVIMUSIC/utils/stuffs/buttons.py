c36a6a2b7ca1255e6be1a9fd1c79a34208c65d87
NainBot <srikanthxd1432@gmail.com>
2025-03-13 09:36:04 +0530

    Update buttons.py

--- PURVIMUSIC/utils/stuffs/buttons.py
+++ PURVIMUSIC/utils/stuffs/buttons.py
@@ -4,8 +4,8 @@ from pyrogram import Client, filters, enums
 class BUTTONS(object):
     ABUTTON = [
     [
-        InlineKeyboardButton("˹ sᴜᴘᴘᴏꝛᴛ ˼", url="https://t.me/+buVXCEkFdfBlYTk9"),
-        InlineKeyboardButton("˹ ᴜᴘᴅᴧᴛᴇ ˼", url="https://t.me/+buVXCEkFdfBlYTk9")
+        InlineKeyboardButton("˹ sᴜᴘᴘᴏꝛᴛ ˼", url="https://t.me/PURVI_SUPPORT"),
+        InlineKeyboardButton("˹ ᴜᴘᴅᴧᴛᴇ ˼", url="https://t.me/+gMy8Cp190ediNzZl")
     ],
     [
         InlineKeyboardButton("˹ sᴏᴜʀᴄᴇ ˼", callback_data="gib_source"),
